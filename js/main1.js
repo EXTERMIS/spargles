3598347a9fe4746ddbcd6f8d42804bc5dc91deca
EXTERMIS <extermis7@gmail.com>
2026-05-05 23:44:29 -0400

    Update main1.js

--- js/main1.js
+++ js/main1.js
@@ -22,10 +22,67 @@ function programs() {
   window.location.assign("https://programs.spargles.com");
 }
 
+function signup() {
+  window.location.assign("https://signup.spargles.com");
+}
+
+function login() {
+  window.location.assign("https://login.spargles.com");
+}
+
 function personal() {
   window.location.assign("https://personal.spargles.com");
 }
 
 function contact() {
   window.location.assign("https://contact.spargles.com");
+}
+
+function terms() {
+  window.location.assign("https://spargles.com/terms");
+}
+
+function BobTool1() {
+  window.location.assign("https://spargles.com/Bobto/Converter");
+}
+
+function PEVideo() {
+  window.location.assign("https://www.youtube.com/watch?v=HNpi5DM8DRA");
+}
+
+const mascotMessages = [
+  "Yo!",
+  "Hola!",
+  "Hello, human!",
+  "Choose BobTo!",
+  "FInd Secrets!",
+  "EST. 2026!",
+  "Glitch Died...",
+  "Pins!!",
+  "Welcome!"
+];
+
+let currentMessageIndex = 0;
+
+function mascotClick(element) {
+  const sound = new Audio("audio/ping.mp3");
+  sound.volume = 0.2;
+  sound.play();
+
+  // animation
+  element.classList.add('active');
+  setTimeout(() => {
+    element.classList.remove('active');
+  }, 200);
+
+  // change text
+  const textElement = document.getElementById("mascot-text");
+
+  textElement.textContent = mascotMessages[currentMessageIndex];
+
+  // move to next message
+  currentMessageIndex++;
+  if (currentMessageIndex >= mascotMessages.length) {
+    currentMessageIndex = 0; // loop back to start
+  }
 }
